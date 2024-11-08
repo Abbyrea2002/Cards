d63d0f092b1dabc6965a3ecb544a5d506b3b47ba
Abbyrea2002 <abbyrea2002@gmail.com>
2024-11-08 22:15:57 +0000

    finished

--- card/src/Snap.java
+++ card/src/Snap.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 /**
  * Created by abbyr on 08/11/2024
  * COMMENTS ABOUT PROGRAM HERE
@@ -5,10 +7,19 @@
 public class Snap
 {
    public static void main(String[] args){
+
       Card myCards[] = new Card [100];
+      int numSnaps = 0;
       for(int i = 0; i<100; i++){
          myCards[i] = new Card();
+         System.out.println(myCards[i]);
+
+         if(i > 0 && myCards.getRankValue().equals(myCards[i-1])){
+            System.out.println("Snap");
+            numSnaps++;
+         }
 
       }
+      System.out.println("There were " + numSnaps + " snaps.");
    }
 }//class

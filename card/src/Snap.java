74d614a1355aa04abbca939a22cb48154f7e37bc
Abbyrea2002 <abbyrea2002@gmail.com>
2024-11-08 19:37:53 +0000

    finished

---
+++ card/src/Snap.java
@@ -0,0 +1,14 @@
+/**
+ * Created by abbyr on 08/11/2024
+ * COMMENTS ABOUT PROGRAM HERE
+ */
+public class Snap
+{
+   public static void main(String[] args){
+      Card myCards[] = new Card [100];
+      for(int i = 0; i<100; i++){
+         myCards[i] = new Card();
+
+      }
+   }
+}//class

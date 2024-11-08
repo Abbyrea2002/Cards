76e8b1561d0bc156e502d3fe2b5000b1621d85dd
Abbyrea2002 <abbyrea2002@gmail.com>
2024-11-08 18:33:06 +0000

    finished

--- card/src/Sort.java
+++ card/src/Sort.java
@@ -27,7 +27,7 @@ public class Sort
             }
          }
          lastPos = lastSwapPos;
-         //System.out.println("Pass : " + Arrays.toString(arr));
+         System.out.println("Pass : " + Arrays.toString(arr));
       }
    }
 
@@ -41,23 +41,71 @@ public class Sort
       return arr;
    }
 
+   public static void selectionSort(int[] arr){
+      int firstPos = 0, lastPos = arr.length - 1;
+      int temp, smallestPos;
+
+      for(int i = firstPos; i<lastPos; i++){
+         smallestPos = i;
+         for(int j=i+1; j<=lastPos; j++){
+            numComparisons++;
+            if(arr[j] < arr[smallestPos]){
+               smallestPos = j;
+            }
+         }
+         temp = arr[smallestPos];
+         arr[smallestPos] = arr[i];
+         arr[i] = temp;
+         numSwaps++;
+         System.out.println("Pass" + (i+1) + ": " + Arrays.toString(arr));
+      }
+   }
+
+   public static void selectionSort_r(int[] arr, int firstPos, int lastPos){
+
+      int temp, smallestPos;
+
+      numComparisons++;
+
+      if(firstPos<lastPos){
+         smallestPos = firstPos;
+         for(int j=firstPos+1; j<=lastPos; j++){
+            numComparisons++;
+            if(arr[j] < arr[smallestPos]){
+               smallestPos = j;
+            }
+         }
+         temp = arr[smallestPos];
+         arr[smallestPos] = arr[firstPos];
+         arr[firstPos] = temp;
+         numSwaps++;
+         System.out.println("Pass" +  ": " + Arrays.toString(arr));
+         selectionSort_r(arr, firstPos+1, lastPos);
+      }
+   }
    public static void main(String[] args){
 
-      int[] arr;
-      long startTime, endTime;
-      int[] arraySizes = {100, 200, 400, 800, 1600, 3200, 6400};
+      int[] arr = {5, 9, 1, 10, 3, 8, 2, 4, 7, 6};
+      System.out.println("Before:" + Arrays.toString(arr));
+      selectionSort_r(arr, 0, arr.length-1);
+      System.out.println("After:" + Arrays.toString(arr));
+      System.out.println("There were " + numComparisons + " comparisons and " + numSwaps + " swaps.");
 
-      for(int arraySize : arraySizes){
-         startTime = System.currentTimeMillis();
-         for(int i=0; i<1000; i++){
-            arr = randomArray(arraySize);
-            bubbleSort(arr);
+      //int[] arr;
+      //long startTime, endTime;
+      //int[] arraySizes = {100, 200, 400, 800, 1600, 3200, 6400};
 
-         }
-         endTime = System.currentTimeMillis();
+//         for(int arraySize : arraySizes){
+//            startTime = System.currentTimeMillis();
+//            for(int i=0; i<1000; i++){
+//               arr = randomArray(arraySize);
+//               bubbleSort(arr);
+//
+//            }
+//            endTime = System.currentTimeMillis();
+//
+//            System.out.println(arraySize + "\t " + (endTime - startTime));
 
-         System.out.println(arraySize + "\t " + (endTime - startTime));
-      }
 
    }
 }//class
